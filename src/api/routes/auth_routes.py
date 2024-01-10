49cdd24741feae200dbc1ce2d1dd79905b44d611
Paul <paul@bree.P>
2024-01-10 11:50:58 +0100

    Adding TodoistAPI to the backend

--- src/api/routes/auth_routes.py
+++ src/api/routes/auth_routes.py
@@ -18,11 +18,19 @@ def authorize():
 @auth_api.route('/callback')
 def callback():
     code = request.args.get('code')
-    token_response = requests.post('https://todoist.com/oauth/access_token', auth=HTTPBasicAuth(client_id, client_secret), data={
-        'code': code,
-        'redirect_uri': redirect_uri,
-        'grant_type': 'authorization_code'
-    }, timeout=10)  # Add timeout argument with a value of 10 seconds
-    access_token = token_response.json().get('access_token')
-    # Store the access token and use it for API requests
+    if not code:
+        return 'Error: No code provided', 400
+    try:
+        token_response = requests.post('https://todoist.com/oauth/access_token', auth=HTTPBasicAuth(client_id, client_secret), data={
+            'code': code,
+            'redirect_uri': redirect_uri,
+            'grant_type': 'authorization_code'
+        }, timeout=10)  # Add timeout argument with a value of 10 seconds
+        token_response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        return f"Error: {str(e)}", 500
+    if token_response.text:
+        access_token = token_response.json().get('access_token')
+    else:
+        return "Error: Empty response from Todoist", 500
     return 'Successfully authenticated'
