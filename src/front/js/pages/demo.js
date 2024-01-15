8eb8b86d3ed4295cdd01ce17be612586531cd6bb
Guido <guidoboninomuelas@gmail.com>
2024-01-15 11:03:36 +0100

    v2

--- src/front/js/pages/demo.js
+++ src/front/js/pages/demo.js
@@ -8,36 +8,7 @@ export const Demo = () => {
 
   return (
     <div className="container">
-      <ul className="list-group">
-        {store.demo.map((item, index) => {
-          return (
-            <li
-              key={index}
-              className="list-group-item d-flex justify-content-between"
-              style={{ background: item.background }}
-            >
-              <Link to={"/single/" + index}>
-                <span>Link to: {item.title}</span>
-              </Link>
-              {
-                // Conditional render example
-                // Check to see if the background is orange, if so, display the message
-                item.background === "orange" ? (
-                  <p style={{ color: item.initial }}>
-                    Check store/flux.js scroll to the actions to see the code
-                  </p>
-                ) : null
-              }
-              <button
-                className="btn btn-success"
-                onClick={() => actions.changeColor(index, "orange")}
-              >
-                Change Color
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+      <h1>Welcome to the awesome CalendApp Alikindoi!</h1>
       <br />
       <Link to="/">
         <button className="btn btn-primary">Back home</button>
