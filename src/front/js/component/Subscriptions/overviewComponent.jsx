1d0316f4b00ddf5f3dd0d8774ade21735b4dff60
María García <139761822+EloraDana1983@users.noreply.github.com>
2024-01-25 11:35:27 +0100

    mensaje

--- src/front/js/component/Subscriptions/overviewComponent.jsx
+++ src/front/js/component/Subscriptions/overviewComponent.jsx
@@ -1,32 +1,5 @@
 import React, { useEffect, useState } from "react";
-import styled, { css } from "styled-components";
-
-const FlipContainer = styled.div`
-  perspective: 1000px;
-`;
-
-const Flipper = styled.div`
-  transition: 0.6s;
-  transform-style: preserve-3d;
-  position: relative;
-
-  ${props => props.isFlipped && css`
-    transform: rotateY(180deg);
-  `}
-`;
-
-const Front = styled.div`
-  position: absolute;
-  width: 100%;
-  backface-visibility: hidden;
-`;
-
-const Back = styled.div`
-  position: absolute;
-  width: 100%;
-  backface-visibility: hidden;
-  transform: rotateY(180deg);
-`;
+import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
@@ -178,31 +151,23 @@ const OverViewComponent = (props) => {
           {isAddTxnVisible ? "CANCEL" : "ADD"}
         </AddTransaction>
       </BalanceBox>
-      <FlipContainer>
-        <Flipper isFlipped={isAddTxnVisible}>
-          <Front>
-            <ExpenseContainer>
-              <ExpenseBox>
-                Expense<span>${props.expense}</span>
-              </ExpenseBox>
-              <ExpenseBox isIncome={true}>
-                Income<span>${props.income}</span>
-              </ExpenseBox>
-            </ExpenseContainer>
-          </Front>
-          <Back>
-            {isAddTxnVisible && (
-              <AddTransactionView
-                isAddTxnVisible={isAddTxnVisible}
-                addTransaction={(payload) => {
-                  props.addTransaction(payload);
-                  toggleAddTXn((isVisible) => !isVisible);
-                }}
-              />
-            )}
-          </Back>
-        </Flipper>
-      </FlipContainer>
+      {isAddTxnVisible && (
+        <AddTransactionView
+          isAddTxnVisible={isAddTxnVisible}
+          addTransaction={(payload) => {
+            props.addTransaction(payload);
+            toggleAddTXn((isVisible) => !isVisible);
+          }}
+        />
+      )}
+      <ExpenseContainer>
+        <ExpenseBox>
+          Expense<span>${props.expense}</span>
+        </ExpenseBox>
+        <ExpenseBox isIncome={true}>
+          Income<span>${props.income}</span>
+        </ExpenseBox>
+      </ExpenseContainer>
     </Container>
   );
 };
